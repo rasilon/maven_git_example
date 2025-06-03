8b90a5adbdf1adc645fde496af56903515972bad
Derry Hamilton <derryh@rasilon.net>
2025-06-03 12:26:48 +0100

    Appears to work

--- src/main/java/com/rasilon/test/SCMTest.java
+++ src/main/java/com/rasilon/test/SCMTest.java
@@ -1,6 +1,24 @@
 package com.rasilon.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class SCMTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        try (InputStream input = SCMTest.class.getClassLoader().getResourceAsStream("git.properties")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            prop.forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
